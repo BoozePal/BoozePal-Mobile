539d9d8805922c5b7cf2616d0646e1ef7561d51e
fannymonori <fannymonori@gmail.com>
2016-11-26 22:44:53 +0100

    request ui

--- boozepal/src/main/java/hu/boozepalmobile/boozepal/adapters/MyRequestRecyclerViewAdapter.java
+++ boozepal/src/main/java/hu/boozepalmobile/boozepal/adapters/MyRequestRecyclerViewAdapter.java
@@ -1,5 +1,6 @@
 package hu.boozepalmobile.boozepal.adapters;
 
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import hu.boozepalmobile.boozepal.R;
+import hu.boozepalmobile.boozepal.fragments.RequestDetailFragment;
 import hu.boozepalmobile.boozepal.fragments.RequestFragment.OnListFragmentInteractionListener;
 import hu.boozepalmobile.boozepal.models.User;
 
@@ -33,7 +35,7 @@ public class MyRequestRecyclerViewAdapter extends RecyclerView.Adapter<MyRequest
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
         holder.mItem = users.get(position);
         //holder.mIdView.setText(mValues.get(position).id);
         //holder.mContentView.setText(mValues.get(position).content);
@@ -44,7 +46,13 @@ public class MyRequestRecyclerViewAdapter extends RecyclerView.Adapter<MyRequest
                 if (null != mListener) {
                     // Notify the active callbacks interface (the activity, if the
                     // fragment is attached to one) that an item has been selected.
-                    mListener.onListFragmentInteraction(holder.mItem);
+                   // mListener.onListFragmentInteraction(holder.user);
+
+                    Intent intent = new Intent(v.getContext(), RequestDetailFragment.class);
+                    intent.putExtra("LOGGED_USER_DATA", user);
+                    intent.putExtra("USER_DATA", users.get(position));
+                    intent.putExtra("TOKEN", token);
+                    v.getContext().startActivity(intent);
                 }
             }
         });
@@ -58,13 +66,17 @@ public class MyRequestRecyclerViewAdapter extends RecyclerView.Adapter<MyRequest
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
-        //public final TextView mIdView;
-        //public final TextView mContentView;
+        public final TextView NameView;
+        public final TextView CityView;
+        public final TextView DateView;
         public User mItem;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
+            NameView = (TextView) view.findViewById(R.id.fragment_request_name);
+            CityView = (TextView) view.findViewById(R.id.fragment_request_city);
+            DateView = (TextView) view.findViewById(R.id.fragment_request_date);
             //mIdView = (TextView) view.findViewById(R.id.id);
             //mContentView = (TextView) view.findViewById(R.id.content);
         }
---
+++ boozepal/src/main/java/hu/boozepalmobile/boozepal/fragments/RequestDetailFragment.java
@@ -0,0 +1,75 @@
+package hu.boozepalmobile.boozepal.fragments;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import hu.boozepalmobile.boozepal.R;
+import hu.boozepalmobile.boozepal.models.User;
+import hu.boozepalmobile.boozepal.utils.CalendarDecorator;
+
+public class RequestDetailFragment extends Fragment {
+    private User userData;
+    private User loggedUser;
+
+    public TextView NameView;
+    public ListView BoozeListView;
+    public ListView PubListView;
+
+    public RequestDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Activity activity = this.getActivity();
+        CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+
+        if(getArguments().containsKey("USER_DATA")){
+            userData = (User) getArguments().getParcelable("USER_DATA");
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(userData.getName());
+            }
+            loggedUser = (User) getArguments().getParcelable("LOGGED_USER_DATA");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.pal_detail, container, false);
+        NameView = (TextView) rootView.findViewById(R.id.NameText);
+        BoozeListView = (ListView) rootView.findViewById(R.id.BoozeListView);
+        PubListView = (ListView) rootView.findViewById(R.id.PubListView);
+
+        if (userData != null) {
+            NameView.setText(userData.getName());
+
+            final ArrayAdapter BoozeAdapter = new ArrayAdapter(getActivity(),
+                    android.R.layout.simple_list_item_1, userData.getBoozes());
+            BoozeListView.setAdapter(BoozeAdapter);
+            final ArrayAdapter PubAdapter = new ArrayAdapter(getActivity(),
+                    android.R.layout.simple_list_item_1, userData.getPubs());
+            PubListView.setAdapter(PubAdapter);
+        }
+
+        return rootView;
+    }
+}
